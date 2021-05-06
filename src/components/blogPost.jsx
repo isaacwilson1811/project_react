f20e83ca42a03429d4adc7bfc37be6ff8d5560a9
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-05-06 09:12:55 -0600

    protected like buttons

--- src/components/blogPost.jsx
+++ src/components/blogPost.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { getFakeText } from '../utils';
-import { Container, Divider, Segment } from 'semantic-ui-react'
+import { Container, Divider, Segment, Button, Icon } from 'semantic-ui-react'
+
+import { useContext } from 'react';
+import { AuthContext } from '../globalContext';
 
 export function BlogPost() {
+
+  const { isAuthed } = useContext(AuthContext);
 
   const [title, setTitle] = useState('Fetching Content');
   const [author, setAuthor] = useState('');
   const [sig, setSig] = useState('');
   const [content, setContent] = useState('');
-  const [commentCount, setCommentCount] = useState(0);
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
 
   useEffect( () => {
     getFakeText(Math.floor(Math.random()*(800-400)+400))
@@ -30,7 +36,25 @@ export function BlogPost() {
         <p>{content.substr(200)}</p>
         <h4>{sig}</h4>
       </Container>
-      <Divider horizontal>{commentCount} Comments</Divider>
+      <Divider horizontal>{likeCount} Likes | {dislikeCount} Dislikes</Divider>
+
+      
+      {isAuthed ? (
+    
+      <div style={{display:'flex',justifyContent:'center'}}>
+        <Button color='teal' icon onClick={()=>setLikeCount(likeCount+1)}>
+          <Icon name='thumbs up outline' />
+        </Button>
+        <Button color='teal' icon onClick={()=>setDislikeCount(dislikeCount+1)}>
+          <Icon name='thumbs down outline' />
+        </Button>
+      </div>
+
+      ) : (
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <h3 style={{color:'red'}} >You must be logged in to like</h3>
+        </div>
+      )}
     </Segment>
   );
 }
